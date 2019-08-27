5479350b562f7dda55e3c795b403573a5c8fb0f5
Sarah Hudspeth <shhudspeth@gmail.com>
2019-08-26 20:12:50 -0700

    Add google api code

--- zhengli/tests/test_ddc_api.py
+++ zhengli/tests/test_ddc_api.py
@@ -2,7 +2,8 @@ import pytest
 
 # from zhengli.core.dewey_decimal_api import get_ddc_api
 from zhengli.core.title_of_book_api import get_ISBN_from_title, get_ddc_api
-
+from zhengli.core.image_title_author_scrape.py import detect_text,
+# tests get_ddc_api with a given ISBN to return a given Dewey Decimal Number
 @pytest.mark.parametrize(
     'ISBN,exp_ddc', [pytest.param(
         '9780980200447', '028/.9'
@@ -11,7 +12,7 @@ def test_ddc_api(ISBN, exp_ddc):
     res_ddc = get_ddc_api(ISBN)
     assert res_ddc == exp_ddc
 
-
+# tests get_ISBN_from_title with a given book title and author, returns ISBN
 @pytest.mark.parametrize(
     'title, author, exp_ISBN', [pytest.param(
         'Silence', 'Shusaku Endo', ['9780720612868', '9780870115356']
@@ -22,7 +23,7 @@ def test_get_ISBN_from_title(title, author, exp_ISBN):
     res_isbn = get_ISBN_from_title(title, author)
     assert res_isbn in exp_ISBN
 
-
+# tests get_ddc_api with given title and author to return Dewey Decimal Number
 @pytest.mark.parametrize(
     'title,, author, exp_ddc', [pytest.param(
         'All the living', 'C. E. Morgan', '813/.6'
@@ -30,3 +31,13 @@ def test_get_ISBN_from_title(title, author, exp_ISBN):
 def test_ddc_api(title, author, exp_ddc):
     res_ddc = get_ddc_api(title=title, author_name=author)
     assert res_ddc == exp_ddc
+
+#
+
+@pytest.mark.parametrize(
+    'path, exp_text_len', [pytest.param(
+        'john_cheever_shelf.png', 1
+    )])
+def test_detect_text(path, exp_text_len):
+    res_text = detect_text(path)
+    assert True if res_text > exp_text_len
