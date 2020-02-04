7a6c8faf43d4afcd4d73a11127c60a7a8b21ee54
shhudspeth <shhudspeth@gmail.com>
2020-02-04 13:11:49 -0800

    Flask + MySQL updates

--- flask-forms/miniform.py
+++ flask-forms/miniform.py
@@ -1,6 +1,7 @@
 # miniform.py
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from werkzeug import secure_filename
 
 # App config.
 DEBUG = True
@@ -15,7 +16,7 @@ class ReusableForm(Form):
     password = TextField('Password:', validators = [validators.required(), \
                                             validators.Length(min=4, max=35)])
 
-
+# Home page, loads a login button and a registration button
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     form = ReusableForm(request.form)
@@ -40,7 +41,12 @@ def hello():
 
     return render_template('index.html', form=form)
 
+# Login Template
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template('login.html', form=form)
 
+# after completing registration sends you to an action and verification page
 @app.route("/registered", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -48,18 +54,48 @@ def register():
         name = request.form['name']
     return(render_template('registered.html', form=form))
 
-@app.route("/upload_pictures/<string:name>", methods=['GET', 'POST'])
-def upload():
-    return(render_template('upload_pictures.html/<name>', form=form))
+# Template for uploading pictures
+@app.route("/upload_pictures/<string:name>/", methods=['GET', 'POST'])
+def upload(name):
+    if request.method=='POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        flash("Upload Sucessful!")
+    return(render_template('upload_pictures.html/', name=name))
 
-@app.route("/recommend", methods=['GET', 'POST'])
-def recommend():
-    return(render_template('recommend.html'))
+# After uploading pictures, this template directs you to the dashboard
+@app.route("/uploaded/", methods=['GET', 'POST'])
+def uploaded(name):
+    if request.method=='POST':
+        f =request.files['file']
+        f.save(secure_filename(f.filename))
 
-@app.route("/home",methods=['GET', 'POST'] )
-def home():
-    return(render_template('index.html'))
+    return(render_template('uploaded.html/', name=name))
 
+# REcommendation Page
+@app.route("/recommend/<string:name>/", methods=['GET', 'POST'])
+def recommend(name):
+    return(render_template('recommend.html/', name=name
+
+# Session and Cookie Template
+@app.route('/save', methods = ["GET","POST"])
+def setcookie():
+    if request.method == "POST":
+        session['user'] = request.form['name']
+        resp = make_response(render_template('dashboard.html', name=session['user']))
+
+        return resp
+
+# Dashboard Template
+@app.route("/dashboard/<name>",methods=['GET', 'POST'] )
+def dash(name):
+    return(render_template('dashboard.html', name=name)
+
+# Delete a session Template
+@app.route('/delete')
+def deleteSession():
+    session.pop('user', None)
+    return ''
 
 if __name__ == "__main__":
     app.run()
--- zhengli/core/sql_create_tables.py
+++ zhengli/core/sql_create_tables.py
@@ -1,58 +1,67 @@
-# mysql_creation.py
-
 import mysql.connector
 import config from config
 
+host_= db-booklib.c5jfpls229sf.us-west-1.rds.amazonaws.com
 
-#creates latest instance of database as still in early prototyping stages
-def create_db(cur):
-    cur.execute('''CREATE DATABASE IF NOT EXISTS BOOKSHELF;
+def connect():
+    """ Connect to the mySQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        db = mysql.connector(connect(host=host_,
+                     user="admin",
+                     passwd="",
+                     db="BOOKSHELF")
+
+        # create a cursor
+        cur = db.cursor()
+        
+        # Config settings
+        SET @@time_zone = 'SYSTEM';
+        SET FOREIGN_KEY_CHECKS=1;
+
+        # execute a statement
+
+        #creates latest instance of database as still in early prototyping stages
+
+        cur.execute('''CREATE DATABASE IF NOT EXISTS BOOKSHELF;
                    USE BOOKSHELF;''')
 
-# creates user_id table
-def create_user_id_tb(cur):
-    cur.execute('''CREATE TABLE IF NOT EXISTS tb_USERS (
-        user_ID integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        user_name varchar (255) UNIQUE,
-        user_email varchar(255) UNIQUE,
-        user_password varchar(255),
-        user_start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-        user_last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-        );''')
+        cur.execute('''CREATE TABLE IF NOT EXISTS tb_USERS (
+                    user_ID integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    user_name varchar (255) UNIQUE,
+                    user_email varchar(255) UNIQUE,
+                    user_password varchar(255),
+                    user_start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                    user_last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+                    );''')
 
-# creates table of book_images
-def create_book_image_tb(cur):
-    cur.execute ('''CREATE TABLE IF NOT EXISTS tb_BOOK_IMAGES(
-        book_images_ID integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        book_info JSON
-        );''')
+        # creates table of book_images
+        cur.execute ('''CREATE TABLE IF NOT EXISTS tb_BOOK_IMAGES(
+                    book_images_ID integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    book_info JSON
+                    );''')
+        # creates table of a bookshelf with userid, imageids and shelfids
+        cur.execute('''CREATE TABLE IF NOT EXISTS tb_SHELF (
+                    shelf_ID integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    user_ID integer NOT NULL,
+                    book_images_ID integer,
+                    book_images_name varchar(255),
+                    CONSTRAINT user_ID FOREIGN KEY(user_ID)
+                    REFERENCES tb_USERS(user_ID),
+                    CONSTRAINT book_images_ID FOREIGN KEY (book_images_ID)
+                    REFERENCES tb_BOOK_IMAGES(book_images_ID));)''')
 
-# creates table of a bookshelf with userid, imageids and shelfids
-def create_shelf_tb(cur):
-    cur.execute('''CREATE TABLE IF NOT EXISTS tb_SHELF (
-        shelf_ID integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        user_ID integer NOT NULL,
-        book_images_ID integer,
-        book_images_name varchar(255),
-        CONSTRAINT user_ID FOREIGN KEY(user_ID)
-        REFERENCES tb_USERS(user_ID),
-        CONSTRAINT book_images_ID FOREIGN KEY (book_images_ID)
-        REFERENCES tb_BOOK_IMAGES(book_images_ID));)''')
+        # commit the db commands
+        db.commit()
 
-def create_bookshelf_db:
-    params = config()
-    db = mysql.connector(connect (**params)
+    except (Exception as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
 
-    cur = db.cursor()
-    # Config settings
-    SET @@time_zone = 'SYSTEM';
-    SET FOREIGN_KEY_CHECKS=1;
 
-    # create database and tables
-    create_db(cur)
-    create_user_id_tb(cur)
-    create_book_image_tb(cur)
-    create_shelf_tb(cur)
-
-    # commit the db commands
-    db.commit()
+if __name__ == '__main__':
+connect()
